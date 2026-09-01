b4bd58ab78c3e1be71fc9543ca7192f02216af56
rch20 <2.14595627e+08+rch20@users.noreply.github.com>
2026-09-01 15:27:19 +0000

    chore: registrar migration de endereco de celula

--- drizzle/0056_cell_address_cep.sql
+++ drizzle/0056_cell_address_cep.sql
@@ -1,7 +1,4 @@
-ALTER TABLE `cells` ADD `addressNumber` varchar(20);
---> statement-breakpoint
-ALTER TABLE `cells` ADD `addressComplement` varchar(120);
---> statement-breakpoint
-ALTER TABLE `cells` ADD `zipCode` varchar(9);
---> statement-breakpoint
+ALTER TABLE `cells` ADD `addressNumber` varchar(20);--> statement-breakpoint
+ALTER TABLE `cells` ADD `addressComplement` varchar(120);--> statement-breakpoint
+ALTER TABLE `cells` ADD `zipCode` varchar(9);--> statement-breakpoint
 ALTER TABLE `cells` ADD `state` varchar(2);
